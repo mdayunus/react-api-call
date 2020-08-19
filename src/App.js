02ae2c7b4288655cd3367179ef3048865eb5e0f7
mdayunus <mdayunus@gmail.com>
2020-08-19 18:22:56 +0530

    making api call in react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      let ran = Math.random() * 100;
+      ran = Math.ceil(ran);
+      fetch("https://api.imgflip.com/get_memes")
+        .then((response) => response.json())
+        .then((data) => {
+          this.setState({
+            img: data.data.memes[ran].url,
+          });
+        })
+        .catch((err) => console.log(err));
+    }, 3000);
+  }
+
+  render() {
+    return this.state.img ? (
+      <img src={this.state.img} alt="problemo" />
+    ) : (
+      <h1>sad</h1>
+    );
+  }
 }
 
 export default App;
